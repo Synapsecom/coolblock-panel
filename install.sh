b552af7a835c65cacbeb14ad9406971e00e928e2
Sotirios Roussis <s.roussis@synapsecom.gr>
2025-03-06 18:14:14 +0200

    Updated docker installation function

--- install.sh
+++ install.sh
@@ -217,6 +217,9 @@ function install_docker() {
         install -m 0755 -d /etc/apt/keyrings
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
         chmod -v a+r /etc/apt/keyrings/docker.asc
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "${VERSION_CODENAME}") stable" \
+            | tee /etc/apt/sources.list.d/docker.list > /dev/null
+        apt update
         apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
         systemctl enable --now docker
     fi
