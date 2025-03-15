30afef41de61d2a4f2a1410feb2614b7dda0a73c
Sotirios Roussis <s.roussis@synapsecom.gr>
2025-03-15 15:46:36 +0200

    Relaxed docker housekeeper

--- housekeeping.sh
+++ housekeeping.sh
@@ -7,6 +7,6 @@ function _log() {
 
 ## Docker stuff
 declare -r dc_result=$(/usr/bin/docker container prune --force 2>&1 | /usr/bin/awk -F ':' '/reclaimed space/{print $2}')
-declare -r di_result=$(/usr/bin/docker image prune --all --force --filter "until=336h" 2>&1 | /usr/bin/awk -F ':' '/reclaimed space/{print $2}')
+declare -r di_result=$(/usr/bin/docker image prune --force --filter "until=168h" 2>&1 | /usr/bin/awk -F ':' '/reclaimed space/{print $2}')
 declare -r dv_result=$(/usr/bin/docker volume prune --force 2>&1 | /usr/bin/awk -F ':' '/reclaimed space/{print $2}')
 _log docker-housekeeping "Containers:${dc_result}, Images:${di_result}, Volumes:${dv_result}"
